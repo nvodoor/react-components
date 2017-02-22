ef786182411dc9171bc9b263b72f9b4e7f57c2ae
Neil Vodoor <nvodoor@gmail.com>
2017-02-22 14:26:38 -0800

    This should commit

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 // TODO
+
+
+class GroceryListItem extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+		done: false
+		};
+	}
+
+	onListItemClick() {
+		this.setState({
+			done: !this.state.done
+		});
+	}
+
+
+	render() {
+
+		var style = {
+			'font-weight': this.state.done ? 'bold' : 'normal'
+		};
+
+		return (
+				<li style={style} onMouseOver={this.onListItemClick.bind(this)}>{this.props.item}</li>
+			);
+	}
+
+}
+
+
+var GroceryList = (props) => (
+	<ul>
+		{props.items.map(item =>
+		<GroceryListItem item={item} />
+		)}
+	</ul>
+);
+
+
+ReactDOM.render(<GroceryList items={['Cucumber', 'Kale']}/>, document.getElementById('app'));
+
+
+
